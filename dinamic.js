c5b14ad1831323357c2dbdfdaeb84d0054484439
AndreeaF <cristina.andreea77@yahoo.com>
2023-07-25 20:48:55 +0300

    first commit

---
+++ dinamic.js
@@ -0,0 +1,17 @@
+let showWinner = document.getElementById("show-winner");
+showWinner.innerHTML = epicFight.findWinner();
+let heroesContainer = document.querySelector(".heroes-container");
+let startFight = document.getElementById("start-fight");
+startFight.addEventListener("click", startFightFunc);
+let showHeroes = document.getElementById("show-heroes");
+showHeroes.addEventListener("click", showHeroesFunc);
+function showHeroesFunc() {
+  heroesContainer.classList.add("d-flex");
+  showHeroes.classList.add("d-none");
+  startFight.classList.add("d-block");
+}
+
+function startFightFunc() {
+  startFight.classList.remove("d-block");
+  showWinner.classList.add("d-block");
+}
